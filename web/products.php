93505ba783cebb1fc377da38f35243c81e8d66e7
projektins <projektins@gmail.com>
2018-10-15 19:42:54 +0200

    modify products

--- web/products.php
+++ web/products.php
@@ -19,13 +19,30 @@ $products = getAllProductsInCategory($categoryid, $_SESSION['lang']);
 <?php require('body.php'); ?>
 <div class="main">
     <h1><?php echo $categoryName; ?></h1>
-    <?php
-    foreach ($products as $product) {
-        echo "<a href=\"product.php?id=$product->id\">link</a><br/>";
-        echo $product;
-        echo "<br/>";
-    }
-    ?>
+    <div class="full-Size">
+        <?php
+        foreach ($products as $product) { ?>
+            <div class="wrapper-small">
+                <div class="product-img-small">
+                    <img src="<?php echo htmlentities($product->__get('image')); ?>" height="420" width="327">
+                </div>
+                <div class="product-info-small">
+                    <div class="product-text-small">
+                        <h1><?php echo htmlentities($product->__get('name')); ?></h1>
+                        <h2><?php echo htmlentities($product->__get('manufacturer')); ?></h2>
+                    </div>
+                    <div class="product-price-btn-small">
+                        <p><span><?php echo htmlentities($product->__get('price')); ?></span>CHF</p>
+                        <?php echo "<a href=\"product.php?id=$product->id\"><button type=\"button\">" ?>
+                        <?php echo getTextForLanguage("PRODUCT") . "</button></a>" ?>
+                    </div>
+                </div>
+            </div>
+            <?php
+            echo "<br/>";
+        }
+        ?>
+    </div>
 </div>
 </body>
 </html>

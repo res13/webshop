817eb2e7dd56ef88af1a5b974bad842e2e9326ae
res13 <andreas.erb@gmx.ch>
2018-10-31 16:27:36 +0100

    using new autoload function

--- web/head.php
+++ web/head.php
@@ -1,13 +1,13 @@
 <?php
-function __autoload($classname) {
+spl_autoload_register(function ($classname) {
     $path = 'to/' . $classname . '.php';
     if (file_exists($path)) {
-        require_once($path);
+        require_once "$path";
         return true;
     } else {
         return false;
     }
-}
+});
 session_start();
 require_once('util/i18n.php');
 require_once('util/util.php');

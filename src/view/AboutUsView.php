7a38bcdf3541bee9f58af1faf6cbb96d4da91cfd
Nik Arm <nik.arm@hotmail.com>
2018-12-26 14:41:39 +0100

    small ajax feature

--- src/view/AboutUsView.php
+++ src/view/AboutUsView.php
@@ -8,8 +8,22 @@ class AboutUsView extends View
         $result = "<body></body><div class=\"main\">
     <h1>" . $languageController->getTextForLanguage("ABOUT_US") . "</h1>
     <p>" . $languageController->getTextForLanguage("PROJECT_DESCRIPTION") . "</p><br/>
-    <p>Flugweg 20</p>
-    <p>3000 Bern</p>
+    <p id='ajaxAdress'>
+        <script>
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById(\"ajaxAdress\").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open(\"GET\", \"resource/adress.txt\", true);
+            xhttp.send();
+        </script>
+        <noscript>
+            Flugweg 20<br/>
+            3000 Bern
+        </noscript>
+    </p>
 </div></body>";
         return $result;
     }
